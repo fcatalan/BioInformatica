a3f50de74380044c1e34d8bcb44c7d00af588a1d
fcatalan <fabcatalan@gmail.com>
2016-12-12 23:13:48 -0300

    Creacion del proyecto de angular 1.5

---
+++ app-bioinformatica/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appBioinformaticaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appBioinformaticaApp
+ */
+angular.module('appBioinformaticaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app-bioinformatica/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appBioinformaticaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appBioinformaticaApp
+ */
+angular.module('appBioinformaticaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
